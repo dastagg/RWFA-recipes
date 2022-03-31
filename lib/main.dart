682842386f0cc1f061bfd631ea61b8147cbdbd66
dastagg:opus5 <dastagg@stagglinux.com>
2022-03-31 12:42:13 -0400

    Clear the screen of the counter app.

--- lib/main.dart
+++ lib/main.dart
@@ -38,41 +38,24 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    // 1
     return Scaffold(
+      // 2
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      // 3
+      body: SafeArea(
+        // TODO: Replace child: Container()
+        // 4
+        child: Container(),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
     );
   }
+
+  // TODO: Add buildRecipeCard() here
 }
+
