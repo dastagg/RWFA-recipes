919bba3bc84317015eb5af58912976f711e28905
dastagg:opus5 <dastagg@stagglinux.com>
2022-03-31 13:35:30 -0400

    Add custom Card to display recipes.

--- lib/main.dart
+++ lib/main.dart
@@ -58,13 +58,43 @@ class _MyHomePageState extends State<MyHomePage> {
           // 6
           itemBuilder: (BuildContext context, int index) {
             // 7
-            // TODO: Update to return Recipe card
-            return Text(Recipe.samples[index].label);
+            // TODO: Add GestureDetector
+            return buildRecipeCard(Recipe.samples[index]);
           },
         ),
       ),
     );
   }
+}
 
-  // TODO: Add buildRecipeCard() here
+Widget buildRecipeCard(Recipe recipe) {
+  return Card(
+    // 1
+    elevation: 2.0,
+    // 2
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+    // 3
+    child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      // 4
+      child: Column(
+        children: <Widget>[
+          Image(image: AssetImage(recipe.imageUrl)),
+          // 5
+          const SizedBox(
+            height: 14.0,
+          ),
+          // 6
+          Text(
+            recipe.label,
+            style: const TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.w700,
+              fontFamily: 'Palatino',
+            ),
+          )
+        ],
+      ),
+    ),
+  );
 }
