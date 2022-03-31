5b9a2a78d2f63d661bbce5b72cdd6f5d9708efa5
dastagg:opus5 <dastagg@stagglinux.com>
2022-03-31 13:52:13 -0400

    Add ingredients and slider bar.

--- lib/recipe_detail.dart
+++ lib/recipe_detail.dart
@@ -15,9 +15,8 @@ class RecipeDetail extends StatefulWidget {
   }
 }
 
-
 class _RecipeDetailState extends State<RecipeDetail> {
-  // TODO: Add _sliderVal here
+  int _sliderVal = 1;
 
   @override
   Widget build(BuildContext context) {
@@ -48,13 +47,44 @@ class _RecipeDetailState extends State<RecipeDetail> {
               widget.recipe.label,
               style: const TextStyle(fontSize: 18),
             ),
-            // TODO: Add Expanded
+            // 7
+            Expanded(
+              // 8
+              child: ListView.builder(
+                padding: const EdgeInsets.all(7.0),
+                itemCount: widget.recipe.ingredients.length,
+                itemBuilder: (BuildContext context, int index) {
+                  final ingredient = widget.recipe.ingredients[index];
+                  // 9
+                  // TODO: Add ingredient.quantity
+                  return Text(
+                      '${ingredient.quantity} ${ingredient.measure} ${ingredient.name}');
+                },
+              ),
+            ),
 
-            // TODO: Add Slider() here
+            Slider(
+              // 10
+              min: 1,
+              max: 10,
+              divisions: 9,
+              // 11
+              label: '${_sliderVal * widget.recipe.servings} servings',
+              // 12
+              value: _sliderVal.toDouble(),
+              // 13
+              onChanged: (newValue) {
+                setState(() {
+                  _sliderVal = newValue.round();
+                });
+              },
+              // 14
+              activeColor: Colors.green,
+              inactiveColor: Colors.black,
+            ),
           ],
         ),
       ),
     );
   }
 }
-
